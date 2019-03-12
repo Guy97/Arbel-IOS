2b1f9792626246e0a0a4a7e98bca24d6d78c5241
Mac <mac@macs-mac-mini.station>
2019-03-12 12:13:06 +0100

    Initial Commit

---
+++ Arbel-IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Arbel-IOS
+//
+//  Created by Mac on 12/03/2019.
+//  Copyright © 2019 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
