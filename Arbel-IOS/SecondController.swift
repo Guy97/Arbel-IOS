8be9cf039bac425df6d0fe5eba2989b71f0eb60a
Simone Ghisu <s.gizzy@live.it>
2019-03-24 14:17:14 +0100

    Bo

--- Arbel-IOS/SecondController.swift
+++ Arbel-IOS/SecondController.swift
@@ -18,7 +18,7 @@ class SecondController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("second")
+        print("second.")
         
     }
     
