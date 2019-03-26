232d2414706d90aaeb722d1054a02bdab65e68d4
Simone Ghisu <s.gizzy@live.it>
2019-03-26 14:04:50 +0100

    Bridge - Calendario

--- Arbel-IOS/Arbel-IOS-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Arbel-IOS-Bridging-Header.h
-//  Arbel-IOS
-//
-//  Created by Mac on 25/03/2019.
-//  Copyright © 2019 Mac. All rights reserved.
-//
-
-#import <FSCalendar/FSCalendar.h>
---
+++ Arbel-IOS/ObjCBridge.h
@@ -0,0 +1,13 @@
+//
+//  ObjCBridge.h
+//  Arbel-IOS
+//
+//  Created by Simone on 26/03/2019.
+//  Copyright © 2019 Mac. All rights reserved.
+//
+
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+
+
+#endif /* ObjCBridge_h */
