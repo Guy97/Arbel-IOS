654c85a7a86a0f24cf27dc68764c02871454abd0
silvanatuku <silvana.tuku@ied.edu>
2019-05-03 11:26:43 +0200

    UI test

--- Arbel-IOS/TestTabController.swift
+++ Arbel-IOS/TestTabController.swift
@@ -9,10 +9,15 @@
 import UIKit
 import XLPagerTabStrip
 
-class TestTabController: UIViewController {
+class TestTabController: UIViewController, UITableViewDelegate {
 
     @IBOutlet weak var searchView: UIView!
     @IBOutlet weak var searchTest: UISearchBar!
+    @IBOutlet weak var testList: UITableView!
+    @IBOutlet weak var addButton: UIButton!
+    
+    var arguments = ["Programmazione", "Graphic Design", "Video Design", "Semiotica dell'arte", "TTPF", "IOS", "Android", "Web Laravel", "Motion Graphic"]
+    
     override func viewDidLoad() {
         elementStyle()
         
@@ -31,8 +36,19 @@ class TestTabController: UIViewController {
         searchView.layer.shadowRadius = 1.4
         searchView.layer.shadowOpacity = 0.3
         self.view.backgroundColor = UIColor(white: 1, alpha: 0)
+        
+        // Styles00
+        addButton.layer.cornerRadius = 8
+        addButton.layer.shadowColor = UIColor (red: 142/255, green: 142/255, blue: 142/255, alpha: 0.5).cgColor
+        addButton.layer.shadowOffset = CGSize (width: 0, height: 1.0)
+        addButton.layer.shadowRadius = 1.1
+        addButton.layer.shadowOpacity = 0.5
+        
+        let insets = UIEdgeInsets(top: 0, left: 0, bottom: 40, right: 0)
+        self.testList.contentInset = insets
     }
 }
+
 
 extension TestTabController : IndicatorInfoProvider {
     func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
